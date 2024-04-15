a397cef4856f2b17dcb9768733a06fec0ad0b711
Yeji Han <yeji.hvn@gmail.com>
2024-04-15 16:32:16 +0900

    Generalized coq path search

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,18 +1,24 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
-vim.g.python3_host_prog = "~/.venv/bin/python3"
--- For Coq
-vim.g.coqtail_coq_path = "~/.opam/default/bin"
 
-vim.g.clipboard = {
-  name = "OSC 52",
-  copy = {
-    ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
-  },
-  paste = {
-    ["+"] = require("vim.ui.clipboard.osc52").paste("+"),
-    ["*"] = require("vim.ui.clipboard.osc52").paste("*"),
-  },
-}
+-- Python3
+vim.g.python3_host_prog = "~/.venv/bin/python3"
+
+-- For Coqtail
+local function get_coq_path()
+  local current_dir = os.getenv("PWD")
+  local opam_dir = current_dir .. "/_opam"
+  local opam_bin = opam_dir .. "/bin"
+
+  local coq_path
+  if os.execute("test -d " .. opam_dir) == 0 then
+    coq_path = opam_bin
+  else
+    coq_path = os.getenv("HOME") .. "/.opam/default/bin"
+  end
+
+  return coq_path
+end
+
+vim.g.coqtail_coq_path = get_coq_path()
